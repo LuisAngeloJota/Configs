839a574900ea2b44e8e6bfd88467d3197172ffbd
Luis Angelo Jota <c82w3b2hu@outlook.com>
2024-04-24 22:26:28 +0800

    Update configs

--- .config/sway/status.sh
+++ .config/sway/status.sh
@@ -10,8 +10,7 @@ time=$(date +"%H:%M")
 speaker=$(wpctl inspect @DEFAULT_AUDIO_SINK@ | awk -F '"' '/node.description/ { print "Speaker: " $2 }')
 
 # Volume
-muted=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{ print $3 }')
-if [ $muted ]
+if [ "$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{ print $3 }')" ]
 then
     volume=$(echo "Volume: Muted")
 else
@@ -21,5 +20,13 @@ fi
 # Brightness
 brightness=$(brightnessctl get | awk '{ print "Brightness: " $1 / 120000 * 100 "%"}')
 
+# Wi-Fi Network Name
+if [ "$(iwctl station list | awk -F '  +' '/wlan/ { print $3 }')" == disconnected ]
+then
+    wifi=$(echo "WiFi: Disconnected")
+else
+    wifi=$(echo "WiFi: Connected")
+fi
+
 # Status Bar
-echo "$speaker   $volume   $brightness   $date   $time"
+echo "$wifi   $speaker   $volume   $brightness   $date   $time"
