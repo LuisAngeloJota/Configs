6a5ce39602b0e4e5dd71983afbd285805866fc02
LuisAngeloJota <c82w3b2hu@outlook.com>
2024-05-07 18:18:41 +0800

    Update steam.nix

--- nixos/steam.nix
+++ nixos/steam.nix
@@ -1,5 +1,9 @@
 { config, pkgs, ... }:
 {
+  environment.systemPackages = with pkgs; [
+    mangohud
+  ];
+
   programs.gamemode.enable = true;
   programs.steam.enable = true;
   programs.steam.gamescopeSession.enable = true;
