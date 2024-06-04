0c2a890f9ff12e4f3acee28020126beadb2a2a5a
LuisAngeloJota <161275998+LuisAngeloJota@users.noreply.github.com>
2024-06-04 14:36:06 +0800

    Update screenrec.sh

--- .config/sway/screenrec.sh
+++ .config/sway/screenrec.sh
@@ -1,7 +1,7 @@
 if [ -z "$(pgrep -x wl-screenrec)" ]; then
-notify-send "Screen Recording: Started"
-wl-screenrec --low-power off --ffmpeg-encoder hevc_nvenc --bitrate "1 MB" --audio --audio-device "$(wpctl inspect @DEFAULT_AUDIO_SINK@ | awk -F '"' '/node.name/ { print $2 ".monitor" }')" -f $HOME/"$(date +"%Y-%m-%d %H:%M:%S").mkv"
+    notify-send "Screen Recording: Started"
+    wl-screenrec --low-power off --ffmpeg-encoder hevc_nvenc --bitrate "1 MB" --audio --audio-device "$(wpctl inspect @DEFAULT_AUDIO_SINK@ | awk -F '"' '/node.name/ { print $2 ".monitor" }')" -f $HOME/"$(date +"%Y-%m-%d %H:%M:%S").mkv"
 else
-notify-send "Screen Recording: Saved to $HOME"
-pkill -x wl-screenrec
+    notify-send "Screen Recording: Saved to $HOME"
+    pkill -x wl-screenrec
 fi
