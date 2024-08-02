d32afda9b41a474fda9fa94e797d70537d60392f
LuisAngeloJota <161275998+LuisAngeloJota@users.noreply.github.com>
2024-08-03 02:33:47 +0800

    Update and rename steam.nix to gaming.nix

--- nixos/gaming.nix
+++ nixos/gaming.nix
@@ -1,10 +1,14 @@
 { config, pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
+    bottles-unwrapped
     mangohud
   ];
 
+  hardware.graphics.enable32Bit = true;
+
   programs.gamemode.enable = true;
+  programs.gamescope.enable = true;
   programs.steam.enable = true;
   programs.steam.gamescopeSession.enable = true;
 }
