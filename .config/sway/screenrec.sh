677336205fc71a1bdaed8ce14df4c03bb8ea7106
LuisAngeloJota <161275998+LuisAngeloJota@users.noreply.github.com>
2024-08-05 15:32:04 +0800

    Delete .config/sway/screenrec.sh

--- .config/sway/screenrec.sh
+++
@@ -1,7 +0,0 @@
-if [ -z "$(pgrep -x wl-screenrec)" ]; then
-    notify-send -t 5000 "Screen Recording: Started"
-    wl-screenrec --low-power off --codec avc --bitrate "2 MB" --audio --audio-device "$(wpctl inspect @DEFAULT_AUDIO_SINK@ | awk -F '"' '/node.name/ { print $2 ".monitor" }')" -f $HOME/"$(date +"%Y-%m-%d %H:%M:%S").mkv"
-else
-    notify-send -t 5000 "Screen Recording: Saved to $HOME"
-    pkill -x wl-screenrec
-fi
