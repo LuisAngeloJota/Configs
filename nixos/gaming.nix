2589cafede343b86155a555460a38f8d1e2cdaaf
LuisAngeloJota <161275998+LuisAngeloJota@users.noreply.github.com>
2024-08-05 09:33:14 +0800

    Update gaming.nix

--- nixos/gaming.nix
+++ nixos/gaming.nix
@@ -8,8 +8,5 @@
   hardware.graphics.enable32Bit = true;
 
   programs.gamemode.enable = true;
-  programs.gamescope.capSysNice = true;
-  programs.gamescope.enable = true;
   programs.steam.enable = true;
-  programs.steam.gamescopeSession.enable = true;
 }
