ccf6a0c1e4d7f53fa3aad76b2ecce1af65847ebe
LuisAngeloJota <161275998+LuisAngeloJota@users.noreply.github.com>
2024-08-25 08:05:38 +0800

    Create bundle.nix

---
+++ nixos/bundle.nix
@@ -0,0 +1,12 @@
+{ config, pkgs, ...}:
+{
+  imports = [
+    #./appimage.nix
+    #./gaming.nix
+    #./idevice-tools.nix
+    #./sway.nix
+    #./thunar.nix
+    #./uxplay.nix
+    #./virt-manager.nix
+  ];
+}
