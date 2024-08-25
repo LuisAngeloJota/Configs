db1164da72e57a5f9154718c939e958f8744603a
LuisAngeloJota <161275998+LuisAngeloJota@users.noreply.github.com>
2024-08-25 17:59:22 +0800

    Update sway.nix

--- nixos/sway.nix
+++ nixos/sway.nix
@@ -27,7 +27,7 @@
 
   services.greetd.enable = true;
   services.greetd.settings = {
-    default_session {
+    default_session = {
       command = "${pkgs.greetd.greetd}/bin/agreety --cmd sway";
     };
   };
