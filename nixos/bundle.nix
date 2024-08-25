d37634687c895e492d1a4742c2bf21289892ef1f
LuisAngeloJota <161275998+LuisAngeloJota@users.noreply.github.com>
2024-08-25 18:38:55 +0800

    Update bundle.nix

--- nixos/bundle.nix
+++ nixos/bundle.nix
@@ -1,12 +1,12 @@
 { config, pkgs, ...}:
 {
   imports = [
-    #./appimage.nix
-    #./gaming.nix
-    #./idevice-tools.nix
-    #./sway.nix
-    #./thunar.nix
-    #./uxplay.nix
-    #./virt-manager.nix
+    ./appimage.nix
+    ./gaming.nix
+    ./idevice-tools.nix
+    ./sway.nix
+    ./thunar.nix
+    ./uxplay.nix
+    ./virt-manager.nix
   ];
 }
