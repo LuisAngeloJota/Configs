6d0138ecd6a069b4c2dc86ea75a25511fcb55c14
LuisAngeloJota <161275998+LuisAngeloJota@users.noreply.github.com>
2024-09-22 18:45:27 +0800

    Update archlinux-pre-install.sh

--- archlinux-pre-install.sh
+++ archlinux-pre-install.sh
@@ -1,3 +1,5 @@
-sudo systemctl enable iwd.service
-sudo systemctl enable systemd-resolved.service
-sudo systemctl enable systemd-timesyncd.service
+systemctl enable iwd.service
+systemctl enable systemd-resolved.service
+systemctl enable systemd-timesyncd.service
+
+git clone --depth 1 https://github.com/LuisAngeloJota/Configs
