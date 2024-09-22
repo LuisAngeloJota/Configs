1595c360126d1d341865da137e8c8e897c7c073c
LuisAngeloJota <161275998+LuisAngeloJota@users.noreply.github.com>
2024-09-22 15:23:48 +0800

    Update archlinux-post-install.sh

--- archlinux-post-install.sh
+++ archlinux-post-install.sh
@@ -9,10 +9,12 @@ makepkg -si
 paru -S adwaita-cursors adwaita-icon-theme aria2 audacious \
             bluez bluez-utils btop \
             chromium code cups \
+            dnsmasq \
+            edk2-ovmf \
             ffmpeg firefox flatpak foot fuzzel \
             gamemode glib2 greetd grim gvfs \
             handbrake \
-            ifuse imagemagick intel-compute-runtime intel-media-driver inter-font iwd \
+            ifuse imagemagick intel-compute-runtime intel-media-driver inter-font iptables-nft iwd \
             libarchive libnotify libreoffice-fresh libvirt \
             mako mangohud mediainfo mpv \
             noto-fonts noto-fonts-cjk noto-fonts-emoji nvidia-open \
@@ -30,3 +32,8 @@ paru -S adwaita-cursors adwaita-icon-theme aria2 audacious \
             zram-generator
 
 flatpak install flathub net.ankiweb.Anki org.localsend.localsend_app net.davidotek.pupgui2
+
+systemctl enable libvirtd.service
+systemctl enable libvirtd.socket
+systemctl enable systemd-networkd.service
+systemctl enable systemd-resolved.service
