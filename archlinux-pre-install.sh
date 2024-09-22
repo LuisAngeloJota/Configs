0f1c5ab13df53e792a6d178194281d234b0cd05b
LuisAngeloJota <161275998+LuisAngeloJota@users.noreply.github.com>
2024-09-22 18:41:46 +0800

    Update archlinux-pre-install.sh

--- archlinux-pre-install.sh
+++ archlinux-pre-install.sh
@@ -1,5 +1,4 @@
 sudo systemctl enable iwd.service
-sudo systemctl enable systemd-networkd.service
 sudo systemctl enable systemd-resolved.service
 sudo systemctl enable systemd-timesyncd.service
 sudo systemctl enable systemd-zram-setup@zram0.service
