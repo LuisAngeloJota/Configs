bda9fc067b2358cf9a6927c745ba88e65369a0e5
LuisAngeloJota <161275998+LuisAngeloJota@users.noreply.github.com>
2024-09-22 12:54:52 +0800

    Create archlinux-post-install.sh

---
+++ archlinux-post-install.sh
@@ -0,0 +1,22 @@
+pacman -Syu
+pacman -S aria2 audacious \
+            bluez bluez-utils btop \
+            chromium code cups \
+            ffmpeg firefox flatpak \
+            git \
+            handbrake \
+            imagemagick intel-compute-runtime intel-media-driver inter-font iwd \
+            libarchive libreoffice-fresh \
+            mediainfo mpv \
+            noto-fonts noto-fonts-cjk noto-fonts-emoji nvidia-open \
+            obs-studio otf-font-awesome \
+            pipewire pipewire-alsa pipewire-jack pipewire-pulse power-profiles-daemon \
+            qpwgraph \
+            rsync rtkit \
+            shotcut steam streamlink \
+            ttf-jetbrains-mono \
+            vpl-gpu-rt \
+            wireguard-tools wireplumber \
+            yt-dlp \
+            zram-generator
+flatpak install flathub net.ankiweb.Anki org.localsend.localsend_app net.davidotek.pupgui2
