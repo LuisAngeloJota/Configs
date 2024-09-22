60426a68dde4274ed98ed7922df1d7a08c4790ce
LuisAngeloJota <161275998+LuisAngeloJota@users.noreply.github.com>
2024-09-22 13:27:19 +0800

    Update archlinux-post-install.sh

--- archlinux-post-install.sh
+++ archlinux-post-install.sh
@@ -9,10 +9,10 @@ paru -S adwaita-cursors adwaita-icon-theme aria2 audacious \
             bluez bluez-utils btop \
             chromium code cups \
             ffmpeg firefox flatpak foot fuzzel \
-            gamemode glib2 greetd grim \
+            gamemode glib2 greetd grim gvfs \
             handbrake \
             ifuse imagemagick intel-compute-runtime intel-media-driver inter-font iwd \
-            libarchive libnotify libreoffice-fresh \
+            libarchive libnotify libreoffice-fresh libvirt \
             mako mangohud mediainfo mpv \
             noto-fonts noto-fonts-cjk noto-fonts-emoji nvidia-open \
             obs-studio otf-font-awesome \
@@ -20,10 +20,12 @@ paru -S adwaita-cursors adwaita-icon-theme aria2 audacious \
             qpwgraph \
             rsync rtkit \
             shotcut slurp steam streamlink sway swaybg \
-            tesseract ttf-jetbrains-mono \
-            vpl-gpu-rt vulkan-validation-layers \
+            tesseract thunar ttf-jetbrains-mono tumbler \
+            uxplay \
+            virt-manager vpl-gpu-rt vulkan-validation-layers \
             wireguard-tools wireplumber wl-clipboard \
             xdg-desktop-portal xdg-desktop-portal-gtk xdg-desktop-portal-wlr \
             yambar yt-dlp \
             zram-generator
+
 flatpak install flathub net.ankiweb.Anki org.localsend.localsend_app net.davidotek.pupgui2
