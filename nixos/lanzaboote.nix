8ceb19e45b8cb9d613187d48642ccfc219b55851
LuisAngeloJota <161275998+LuisAngeloJota@users.noreply.github.com>
2024-10-23 17:02:57 +0800

    Update lanzaboote.nix

--- nixos/lanzaboote.nix
+++ nixos/lanzaboote.nix
@@ -8,10 +8,6 @@ in
     lanzaboote.nixosModules.lanzaboote
   ];
 
-  environment.systemPackages = with pkgs; [
-    sbctl
-  ];
-
   boot.loader.systemd-boot.enable = lib.mkForce false;
   boot.lanzaboote.enable = true;
   boot.lanzaboote.pkiBundle = "/etc/secureboot";
